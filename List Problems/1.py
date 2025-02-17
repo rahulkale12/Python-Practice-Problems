6f033fe3d5ca813a957dff8658dad7396c93a080
rahulkale12 <work.rk12@gmail.com>
2025-02-17 17:14:36 +0530

    code

---
+++ List Problems/1.py
@@ -0,0 +1,18 @@
+#Create a Python program to find the sum and average of all elements in a list.
+a = [10,20,30,40,50]
+b = sum(a)
+c = len(a)
+print(b) #sum
+print(b/c) #average
+
+
+#using function
+
+# def average_sum(arr):
+#     total = sum(arr)
+#     average = total/len(arr) if arr else 0
+#     return total,average
+
+# total,average=average_sum([10,20,30,40,50])
+# print("Sum is:",total)
+# print("Average is:",average)
---
+++ List Problems/2.py
@@ -0,0 +1,13 @@
+#Reverse a List Without Using reverse() Function:
+
+a = [1,2,4,2,5]
+print(a[::-1])
+
+
+#using function
+
+# def reverse(arr):
+#     return arr[::-1]
+
+# arr = [1,2,3,4,5]
+# print(reverse(arr))
